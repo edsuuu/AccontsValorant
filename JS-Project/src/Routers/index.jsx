0e2f05c5950db18471cdc9d1e4f17daed33b2aed
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-19 17:56:53 -0300

    implementation router private

--- JS-Project/src/Routers/index.jsx
+++ JS-Project/src/Routers/index.jsx
@@ -4,22 +4,21 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "../Pages/HomePage";
 import Login from "../Pages/Login";
 import Perfil from "../Pages/Profile";
-// import Register from "../Pages/Register";
 import Redirect from "../Pages/Redirect";
 import Pagina404 from "../Pages/NotFoundPage";
-
-// import Logado from "../Pages/Logado";
 import PrivateRoute from "./PrivateRoute";
 import Contas from "../Pages/Contas";
 import CreateAndEditAccount from "../Pages/CreateAndEditAccount";
+import AdminPanel from "../Pages/Admin";
+import Unauthorized from "../Pages/Unauthorized";
 
 export function AppRoutes() {
      return (
           <Routes>
                <Route path="/" element={<HomePage />} />
                <Route path="/login" element={<Login />} />
-               {/* <Route path="/register" element={<Register />} /> */}
                <Route path="/redirect" element={<Redirect />} />
+               <Route path="/unauthorized" element={<Unauthorized />} />
 
                <Route
                     path="/cadastrar-conta"
@@ -45,12 +44,22 @@ export function AppRoutes() {
                          </PrivateRoute>
                     }
                />
-
                <Route
                     path="/perfil"
                     element={
                          <PrivateRoute isClosed={true}>
                               {() => <Perfil />}
+                         </PrivateRoute>
+                    }
+               />
+               <Route
+                    path="/admin"
+                    element={
+                         <PrivateRoute
+                              isClosed={true}
+                              requiredPermission="admin"
+                         >
+                              {() => <AdminPanel />}
                          </PrivateRoute>
                     }
                />
