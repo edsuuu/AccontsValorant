3107fc6dcdb0aa84a67ede5b43e8a61693ae6f98
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-19 17:56:13 -0300

    Add Private Router for permission

--- JS-Project/src/Routers/PrivateRoute.jsx
+++ JS-Project/src/Routers/PrivateRoute.jsx
@@ -1,29 +1,44 @@
 import React from "react";
-import { Navigate, useLocation  } from "react-router-dom";
-import PropTypes from 'prop-types';
+import { Navigate, useLocation } from "react-router-dom";
+import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 
-export default function MyRoute({ children, isClosed }) {
-     const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+export default function MyRoute({ children, isClosed, requiredPermission }) {
+     const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+     const userPermission = useSelector((state) => state.auth.user.permission);
+
      const location = useLocation();
 
      if (isClosed && !isLoggedIn) {
           return (
                <Navigate
-               to={{
-                    pathname: "/redirect",
-                    state: { prevPath: location.pathname }
-               }}
+                    to={{
+                         pathname: "/redirect",
+                         state: { prevPath: location.pathname },
+                    }}
                />
           );
      }
 
-     const renderedChildren = typeof children === 'function' ? children() : children;
+     if (requiredPermission && userPermission !== requiredPermission) {
+          return (
+               <Navigate
+                    to={{
+                         pathname: "/unauthorized",
+                         state: { prevPath: location.pathname },
+                    }}
+               />
+          );
+     }
+
+     const renderedChildren =
+          typeof children === "function" ? children() : children;
 
      return isLoggedIn ? renderedChildren : <Navigate to="/login" />;
 }
 
 MyRoute.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.func, PropTypes.node]).isRequired,
-  isClosed: PropTypes.bool,
+     children: PropTypes.oneOfType([PropTypes.func, PropTypes.node]).isRequired,
+     isClosed: PropTypes.bool,
+     requiredPermission: PropTypes.string,
 };
