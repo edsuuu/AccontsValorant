f4c73fb53190f4215ea9e9b59cdf33154a2ad903
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-21 21:46:14 -0300

    Update index.tsx

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -9,8 +9,9 @@ import * as actions from '../../store/modules/auth/actions';
 import { RootState } from '../../store/modules/rootReducer';
 import { AppDispatch } from '../../store';
 import { FaLinkedin } from 'react-icons/fa';
-import { FaFacebook } from "react-icons/fa";
-import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+import Loading from '../../Components/Loading';
 
 const Login: React.FC = () => {
     const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
@@ -51,18 +52,22 @@ const Login: React.FC = () => {
 
     return (
         <Container>
+            <Loading isLoading={isLoading} />
             <div className="container" id="container">
                 <div className="form-container sign-up-container">
                     <form action="#">
                         <h1>Crie sua conta!</h1>
                         <div className="social-container">
-                            <a href="#" className="social"><FaFacebook /><i className="fab fa-facebook-f"></i>
+                            <a href="#" className="social">
+                                <FaFacebook />
+                                <i className="fab fa-facebook-f"></i>
                             </a>
-                            <a href="#" className="social"><FcGoogle />
-                                <i className="fab fa-google-plus-g">
-                                </i>
+                            <a href="#" className="social">
+                                <FcGoogle />
+                                <i className="fab fa-google-plus-g"></i>
                             </a>
-                            <a href="#" className="social"><FaLinkedin />
+                            <a href="#" className="social">
+                                <FaLinkedin />
                                 <i className="fab fa-linkedin-in"></i>
                             </a>
                         </div>
@@ -74,23 +79,25 @@ const Login: React.FC = () => {
                     </form>
                 </div>
                 <div className="form-container sign-in-container">
-                    <form action="#">
+                    <form onSubmit={handleSubmit}>
                         <h1>Entre</h1>
                         <div className="social-container">
-                        <a href="#" className="social"><FaFacebook />
+                            <a href="#" className="social">
+                                <FaFacebook />
                                 <i className="fab fa-facebook-f"></i>
                             </a>
-                            <a href="#" className="social"><FcGoogle />
-                                <i className="fab fa-google-plus-g">
-                                </i>
+                            <a href="#" className="social">
+                                <FcGoogle />
+                                <i className="fab fa-google-plus-g"></i>
                             </a>
-                            <a href="#" className="social"><FaLinkedin />
+                            <a href="#" className="social">
+                                <FaLinkedin />
                                 <i className="fab fa-linkedin-in"></i>
                             </a>
                         </div>
                         <span> ou use sua conta</span>
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Senha" />
+                        <input type="text" value={login} onChange={(e) => setLogin(e.target.value)} placeholder="Digite seu Login" />
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Digite a sua Senha" />
                         <a href="#">Esqueceu a senha?</a>
                         <button>Entrar</button>
                     </form>
