6aca75ee3c57c4f8bdb704cc1c28715aa924fd5b
João Victor <88406996+joaosouzaa01@users.noreply.github.com>
2024-07-02 21:54:49 -0300

    Update index.tsx

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Container } from './styled';
 
 const Home: React.FC = () => {
     return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-        </div>
+        <Container>
+            <h1>Bem vindo!</h1>
+        </Container>
     );
 };
 
