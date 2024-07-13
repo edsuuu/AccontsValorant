e48c9477253cc126552ab7bd05d57bb489902a7a
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-07-12 22:00:45 -0300

    Update

---
+++ src/Pages/Profile/styled.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: wheat;
+    width: 700px;
+    text-align: center;
+    margin: 4rem auto;
+`;
+
+export const Informacoes = styled.div`
+    border: 1px solid black;
+`;
